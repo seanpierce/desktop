bf017860a1c74cace8d2722399dfd971a16664f8
seanpierce <contact@seanpierce.us>
2017-09-10 15:25:50 -0700

    expand upon "item as an object" idea

---
+++ scripts/item_content.js
@@ -0,0 +1,23 @@
+const ITEMS = [
+  {
+    "name" : "press",
+    "content" : {
+      "files" : ["interview_with_zosia_wiatr.txt", "joel_shanahan.txt", "eric_evans.txt"],
+      "directories" : []
+    }
+  },
+  {
+    "name" : "images",
+    "content" : {
+      "files" : [],
+      "directories" : []
+    }
+  },
+  {
+    "name" : "projects",
+    "content" : {
+      "files" : [],
+      "directories" : ["introtorhythm", "spoiler room"]
+    }
+  }
+];
