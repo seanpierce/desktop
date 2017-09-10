6f54c73aa94c46260ab9c96d132c987df6d0b317
seanpierce <contact@seanpierce.us>
2017-09-10 16:00:24 -0700

    refactor items layout

--- scripts/item_content.js
+++ scripts/item_content.js
@@ -2,21 +2,34 @@ const ITEMS = [
   {
     "name" : "press",
     "content" : {
-      "files" : ["interview_with_zosia_wiatr.txt", "joel_shanahan.txt", "eric_evans.txt"],
+      "files" : [
+        { "name" : "interview_with_zosia_wiatr.txt" },
+        { "name" : "joel_shanahan.txt" },
+        { "name" : "eric_evans.txt" }
+      ],
       "directories" : []
     }
   },
   {
     "name" : "images",
     "content" : {
-      "files" : [],
+      "files" : [
+        { "name" : "sean_at_dbfest.jpg" },
+        { "name" : "screenshot 09/15/1984.png" },
+        { "name" : "sean_and_lizi.jpg" },
+        { "name" : "39881rst2.jpg" }
+      ],
       "directories" : []
     }
   },
   {
     "name" : "projects",
     "content" : {
-      "files" : [],
+      "files" : [
+        { "name" : "spoiler room" },
+        { "name" : "introtorhythm.com" },
+        { "name" : "ASSS" }
+      ],
       "directories" : ["introtorhythm", "spoiler room"]
     }
   }
