3b746b31e26a7d33b8587bd865523bbb925a9abb
seanpierce <sean@planetargon.com>
2017-09-11 16:19:42 -0700

    refctor logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,3 @@
 $(function() {
 
-  // make folders draggable
-  $('.folder').draggable({
-    // during drag
-    drag: function() {
-      // remove selected status from all folders
-      $('.folder').removeClass('selected');
-      // add selected class
-      $(this).addClass('selected');
-    }
-  });
-
-  // highlight selected folder on click
-  $('.folder').click(function() {
-    // remove selected status from all folders
-    $('.folder').removeClass('selected');
-    // apply selected status only to clicked folder
-    $(this).toggleClass('selected');
-  });
-
-  // remove selected status from all folders
-  //  when user clicks desktop
-  $('#background-div').click(function() {
-    $('.folder').removeClass('selected');
-  });
-
 });
--- scripts/folder.js
+++ scripts/folder.js
@@ -2,7 +2,7 @@ var folder = function(name, type) {
 
 let html =
 `
-<div class="folder" data-folder="${name}">
+<div class="folder ${type}__true" data-folder="${name}">
   <img src="assets/folder-${type}.png" alt="">
   <figcaption>
     ${name}
@@ -14,9 +14,35 @@ return html;
 };
 
 $(function() {
+  // render all desktop items
   for(var i=0; i < ITEMS.length; i++) {
     let name = ITEMS[i].name;
     let type = ITEMS[i].type;
     $('body').append(folder(name, type));
   }
+
+  // make folders draggable
+  $('.folder').draggable({
+    // during drag
+    drag: function() {
+      // remove selected status from all folders
+      $('.folder').removeClass('selected');
+      // add selected class
+      $(this).addClass('selected');
+    }
+  });
+
+  // highlight selected folder on click
+  $('.folder').click(function() {
+    // remove selected status from all folders
+    $('.folder').removeClass('selected');
+    // apply selected status only to clicked folder
+    $(this).toggleClass('selected');
+  });
+
+  // remove selected status from all folders
+  //  when user clicks desktop
+  $('#background-div').click(function() {
+    $('.folder').removeClass('selected');
+  });
 });
